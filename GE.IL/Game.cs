178268ec736f43014035728cca8ab5a60780b7c0
simlex <simlex.dev.@gmail.com>
2016-01-14 17:05:21 +0400

    fghfgh

---
+++ GE.IL/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GE.IL
+{
+    public class Game
+    {
+        public string Title { get; set; }
+        public Picture FrontPicture { get; set; }
+        public Picture Icon { get; set; }
+    }
+}
--- src/GE/ViewModels/VMGameMenu.cs
+++ src/GE/ViewModels/VMGameMenu.cs
@@ -8,10 +8,28 @@ namespace GE.ViewModels
     public class VMGameMenu
     {
         public VMGameMenuGame CurrentGame { get; set; }
+        public VMGameMenuImg GoodGame { get; set; }
+        public VMGameMenuImg BadGame { get; set; }
+        public List<VMGameMenuGame> Games { get; set; }
+        public string MenuItemWidth
+        {
+            get
+            {
+                var result = Games.Any() ? Math.Round(100 / (decimal)Games.Count, 4) : 0;
+                return result + "px";
+            }
+        }
     }
 
     public class VMGameMenuGame
     {
+        public string PictureImgSrc { get; set; }
+        public string Title { get; set; }
+    }
 
+    public class VMGameMenuImg
+    {
+        public string Title { get; set; }
+        public string ImgSrc { get; set; }
     }
 }

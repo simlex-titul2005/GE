d94edfe0d2d3b49fddb8a60e0a5fe5542dbf62db
simlex <simlex.dev.2014@gmail.com>
2016-01-15 01:41:16 +0300

    Доработки

--- src/GE/gulpfile.js
+++ src/GE/gulpfile.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var gulp = require('gulp'),
+    concat = require('gulp-concat'),
     less = require('gulp-less');
 
 var paths = {
@@ -9,7 +10,12 @@ var paths = {
 };
 
 gulp.task("less", function () {
-    //return gulp.src([paths.webroot + 'less/fonts.less'])
+    //return gulp.src([
+    //    paths.webroot + 'less/fonts.less',
+    //    paths.webroot + 'less/bootstrap-reset.less',
+    //    paths.webroot + 'less/app.less',
+    //    paths.webroot + 'less/ge-games-menu.less'
+    //])
     //        .pipe(less())
     //        .pipe(concat('less.less'))
     //        .pipe(gulp.dest(paths.webroot + 'dist/css/app.min.css'))

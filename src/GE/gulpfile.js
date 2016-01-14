6a09438f8e20f3e2e0c5fe42878dbefdf2837a0c
simlex <simlex.dev.@gmail.com>
2016-01-14 14:29:19 +0400

    Css

---
+++ src/GE/gulpfile.js
@@ -0,0 +1,44 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require('gulp'),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    less = require("gulp-less");
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+
+/*clean*/
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.webroot + 'css/**/app*.css', cb);
+});
+
+gulp.task("clean", ["clean:css"]);
+
+/*less fiels to css*/
+gulp.task("less", function () {
+    gulp.src([paths.webroot + 'less/reset.less', paths.webroot + 'less/app.less'])
+        .pipe(concat('app.less'))
+        .pipe(less())
+        .pipe(gulp.dest(paths.webroot + 'css'))
+});
+
+/*min files*/
+gulp.task("min:css", function () {
+    return gulp.src([paths.webroot + 'css/app.css', paths.webroot + 'css/fonts.css'])
+        .pipe(concat(paths.webroot+'css/app.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:css"]);
+
+/*run*/
+gulp.task("min", ["clean", "less", "min:css"]);

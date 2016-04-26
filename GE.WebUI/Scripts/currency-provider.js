b36ac12d0b6e78217ab30dc681553ba3cf7d5369
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-04-26 11:32:05 +0300

    xxx

--- GE.WebUI/Scripts/currency-provider.js
+++ GE.WebUI/Scripts/currency-provider.js
@@ -1,19 +1,28 @@
 ﻿$(function () {
-    $('a[data-currency-cc]').click(function () {
-        getCurrency(this);
+    $('a[data-currency-cc]').each(function () {
+        $(this).tooltip();
+        $(this).one('click', function () {
+            getCurrency(this);
+        });
     });
 });
 
 //получить курс валюты
-function getCurrency(s, e)
-{
+function getCurrency(s, e) {
     var charCode = $(s).data('currency-cc');
+    var value = parseFloat($(s).data('value').replace(/ /g,''));
     $.ajax({
         url: '/valutes/getcurcourse',
         data: { cc: charCode },
         method: 'post',
         success: function (data) {
-            $(s).html(data.Value + ' <i class="fa fa-rub"></i>');
+            $(s).html(getCurrencyString(Math.round(data.Value * value, 0)) + ' <i class="fa fa-rub"></i>');
+            $(s).tooltip('destroy');
         }
     });
+}
+
+function getCurrencyString(n)
+{
+    return n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ');
 }
--- SX.WebCore/SxBBCodeParser.cs
+++ SX.WebCore/SxBBCodeParser.cs
@@ -8,9 +8,9 @@ namespace SX.WebCore
         {
             //currency
             Regex re = new Regex(@"\[USD\](.*?)\[\/USD\]");
-            var res = re.Replace(inputHtml, "<a href=\"javascript:void(0)\" data-currency-cc=\"USD\">$1 <i class=\"fa fa-usd\"></i></a>");
+            var res = re.Replace(inputHtml, "<a title=\"Узнать курс в рублях\" class=\"currency\" href=\"javascript:void(0)\" data-value=\"$1\" data-currency-cc=\"USD\">$1 <i class=\"fa fa-usd\"></i></a>");
             re = new Regex(@"\[EUR\](.*?)\[\/EUR\]");
-            res = re.Replace(res, "<a href=\"javascript:void(0)\" data-currency-cc=\"EUR\">$1 <i class=\"fa fa-eur\"></i></a>");
+            res = re.Replace(res, "<a title=\"Узнать курс в рублях\" class=\"currency\" href=\"javascript:void(0)\" data-value=\"$1\" data-currency-cc=\"EUR\">$1 <i class=\"fa fa-eur\"></i></a>");
 
             return res;
         }

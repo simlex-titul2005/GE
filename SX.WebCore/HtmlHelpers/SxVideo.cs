d28ca0e33d602d5df869c756bc6dd88f1cd48d65
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-05-13 16:12:35 +0300

    video

--- SX.WebCore/HtmlHelpers/SxVideo.cs
+++ SX.WebCore/HtmlHelpers/SxVideo.cs
@@ -12,17 +12,22 @@ namespace SX.WebCore.HtmlHelpers
 
         public static string GetVideoTemplate(SxVideo video)
         {
+            var id = video.Id.ToString().ToLower();
             var figure = new TagBuilder("figure");
             figure.AddCssClass("video");
             figure.MergeAttribute("style", string.Concat("background-image:url(", VideoProvider.GetVideoImageUrl(video.VideoId), ");"));
+            figure.MergeAttribute("id", id);
 
             var playWrapper = new TagBuilder("div");
             playWrapper.AddCssClass("v-p-wr");
 
             var a = new TagBuilder("a");
             a.MergeAttribute("href", "javascript:void(0)");
+            a.MergeAttribute("onclick", string.Concat("playVideoById('", id, "','", video.VideoId,"')"));
             a.InnerHtml += "<i class=\"fa fa-youtube-play\"></i>";
             playWrapper.InnerHtml += a;
+
+            figure.InnerHtml += playWrapper;
 
             return figure.ToString();
         }

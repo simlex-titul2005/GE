a8a2326db3766329309904caa6dfb296807b6ce7
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-07-14 10:37:58 +0300

    1.0.0.10

--- GE.WebAdmin/Controllers/SiteTestsController.cs
+++ GE.WebAdmin/Controllers/SiteTestsController.cs
@@ -1,9 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using GE.WebCoreExtantions;
+using SX.WebCore.MvcControllers;
+using System.Web.Mvc;
 
 namespace GE.WebAdmin.Controllers
 {
     [Authorize(Roles = "admin")]
-    public partial class SiteTestsController : SX.WebCore.MvcControllers.SxSiteTestsController<WebCoreExtantions.DbContext>
+    public partial class SiteTestsController : SxSiteTestsController<DbContext>
     {
         
     }

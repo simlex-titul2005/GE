929b00615890747855cc73fd9150e41ea5e3d0a6
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-10-26 17:06:01 +0300

    2.1

--- GE.WebUI/Controllers/SeoController.cs
+++ GE.WebUI/Controllers/SeoController.cs
@@ -1,4 +1,5 @@
 ﻿using GE.WebUI.ViewModels;
+using GE.WebUI.ViewModels.Abstracts;
 using SX.WebCore.MvcControllers;
 using System;
 using static SX.WebCore.Enums;
@@ -11,26 +12,48 @@ namespace GE.WebUI.Controllers
         {
             get
             {
-                return getSeoItemUrl;
+                return (model) =>
+                {
+                    var mct= (byte)model.ModelCoreType;
+
+                    VMMaterial item = null;
+                    switch(mct)
+                    {
+                        case 1:
+                            item= new VMArticle { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct };
+                            break;
+                        case 2:
+                            item = new VMNews { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct };
+                            break;
+                        case 6:
+                            item = new VMAphorism { TitleUrl = model.TitleUrl, CategoryId = model.CategoryId, ModelCoreType = mct };
+                            break;
+                        case 7:
+                            item = new VMHumor { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct };
+                            break;
+                    }
+
+                    return item?.GetUrl(Url) ?? null;
+                };
             }
         }
 
-        private string getSeoItemUrl(dynamic model)
-        {
-            var mct = (byte)model.ModelCoreType;
-            switch (mct)
-            {
-                case (byte)ModelCoreType.Article:
-                    return new VMArticle { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct }.GetUrl(Url);
-                case (byte)ModelCoreType.News:
-                    return new VMNews { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct }.GetUrl(Url);
-                case 6://aphorism
-                    return new VMAphorism { TitleUrl=model.TitleUrl, CategoryId=model.CategoryId, ModelCoreType=mct }.GetUrl(Url);
-                case 7://humor
-                    return new VMHumor { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct }.GetUrl(Url);
-                default:
-                    return null;
-            }
-        }
+        //private string getSeoItemUrl(dynamic model)
+        //{
+        //    var mct = (byte)model.ModelCoreType;
+        //    switch (mct)
+        //    {
+        //        case (byte)ModelCoreType.Article:
+        //            return new VMArticle { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct }.GetUrl(Url);
+        //        case (byte)ModelCoreType.News:
+        //            return new VMNews { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct }.GetUrl(Url);
+        //        case 6://aphorism
+        //            return new VMAphorism { TitleUrl=model.TitleUrl, CategoryId=model.CategoryId, ModelCoreType=mct }.GetUrl(Url);
+        //        case 7://humor
+        //            return new VMHumor { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct }.GetUrl(Url);
+        //        default:
+        //            return null;
+        //    }
+        //}
     }
 }

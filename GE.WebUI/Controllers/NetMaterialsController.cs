67d30620a992fe22717c9478c4ff19dbe3606d83
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-11-16 17:05:13 +0200

    2.1

---
+++ GE.WebUI/Controllers/NetMaterialsController.cs
@@ -0,0 +1,10 @@
+﻿using SX.WebCore.MvcControllers;
+using SX.WebCore.ViewModels;
+
+namespace GE.WebUI.Controllers
+{
+    public sealed class NetMaterialsController : SxNetMaterialsController<SxVMMaterial>
+    {
+        
+    }
+}

567acfe4145cfe96acc8c286909d5bb94262abe8
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-12-29 11:57:19 +0200

    2.4

--- GE.WebUI/Infrastructure/DbContext.cs
+++ GE.WebUI/Infrastructure/DbContext.cs
@@ -19,6 +19,8 @@ namespace GE.WebUI.Infrastructure
 
         public DbSet<Humor> Humors { get; set; }
 
+        public DbSet<Infographic> Infographics { get; set; }
+
         public new DbSet<News> News { get; set; }
 
         public DbSet<PopularYoutubeVideo> PopularYoutubeVideos { get; set; }
---
+++ GE.WebUI/Models/Infographic.cs
@@ -0,0 +1,22 @@
+﻿using SX.WebCore.DbModels;
+using SX.WebCore.DbModels.Abstract;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GE.WebUI.Models
+{
+    [Table("D_INFOGRAPHIC")]
+    public class Infographic
+    {
+        [Key, Column(Order = 1)]
+        public Guid Id { get; set; }
+        public virtual SxPicture Picture { get; set; }
+
+        [Key, Column(Order = 2)]
+        public int MaterialId { get; set; }
+        [Key, Column(Order = 3)]
+        public byte ModelCoreType { get; set; }
+        public virtual SxMaterial Material { get; set; }
+    }
+}
